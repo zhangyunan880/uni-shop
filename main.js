e1d4d0972341f99930b2a4035f5063508cabfbe9
zhangyunan <1345718600@qq.com>
2022-12-05 18:27:51 +0800

    完成首页

--- main.js
+++ main.js
@@ -3,6 +3,31 @@
 import Vue from 'vue'
 import App from './App'
 
+import {$http} from "@escook/request-miniprogram"
+uni.$http=$http
+
+$http.baseUrl='https://www.uinav.com'
+
+$http.beforeRequest = function (options) {
+   uni.showLoading({
+   	title:'数据加载中...'
+   })
+}
+
+$http.afterRequest = function () {
+  uni.hideLoading()
+}
+
+
+uni.$showMsg=function(title="数据请求失败",duration=1500){
+	uni.showToast({
+		title,
+		duration,
+		icon:'none'
+	})
+}
+
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,19 +1,156 @@
 <template>
 	<view>
-		
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+			<swiper-item v-for="(item,index) in swiperlist" :key="index">
+				<navigator class="swiper-item" :url="`/subpkg/goods_datail/goods_datail?goods_id=`+item.goods_id">
+					<image :src="item.image_src" mode=""></image>
+				</navigator>
+			</swiper-item>
+
+		</swiper>
+
+		<view class="nav-list">
+			<view class="nav-item" v-for="(item, i) in navlist" :key="i" @click="navClickHandler(item)">
+				<image :src="item.image_src" class="nav-img"></image>
+			</view>
+		</view>
+
+		<!-- 楼层区域 -->
+		<view class="floor-list">
+			<!-- 楼层 item 项 -->
+			<view class="floor-item" v-for="(item, i) in floorList" :key="i">
+				<!-- 楼层标题 -->
+				<image :src="item.floor_title.image_src" class="floor-title"></image>
+				<!-- 楼层图片区域 -->
+				<view class="floor-img-box">
+					<!-- 左侧大图片的盒子 -->
+					<navigator class="left-img-box" :url="item.product_list[0].url">
+						<image :src="item.product_list[0].image_src"
+							:style="{width: item.product_list[0].image_width + 'rpx'}" mode="widthFix"></image>
+					</navigator>
+					<!-- 右侧 4 个小图片的盒子 -->
+					<view class="right-img-box">
+						<navigator class="right-img-item" v-for="(item2, i2) in item.product_list" :key="i2" v-if="i2 !== 0" :url="item2.url">
+							<image :src="item2.image_src" mode="widthFix" :style="{width: item2.image_width + 'rpx'}">
+							</image>
+						</navigator>
+					</view>
+				</view>
+			</view>
+
+
+		</view>
+
+
 	</view>
 </template>
 
 <script>
+	import {
+		$http
+	} from '@escook/request-miniprogram';
 	export default {
 		data() {
 			return {
-				
+				//轮播
+				swiperlist: [],
+				navlist: [],
+				floorList: [],
+
 			};
-		}
+		},
+		onLoad() {
+			this.getswiperlist()
+			this.getnavlist()
+			this.getFloorList()
+		},
+		methods: {
+			async getswiperlist() {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/swiperdata')
+				if (res.meta.status !== 200) return uni.$showMsg()
+				this.swiperlist = res.message
+
+			},
+
+			async getnavlist() {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/catitems')
+				if (res.meta.status !== 200) return uni.$showMsg()
+				this.navlist = res.message
+			},
+
+			navClickHandler(item) {
+				// 判断点击的是哪个 nav
+				if (item.name === '分类') {
+					uni.switchTab({
+						url: '/pages/cate/cate'
+					})
+				}
+			},
+
+			async getFloorList() {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/floordata')
+				if (res.meta.status !== 200) return uni.$showMsg()
+				res.message.forEach(floor=>{
+					floor.product_list.forEach(prod=>{
+						prod.url ='/subpkg/goods_list/goods_list?'+prod.navigator_url.split('?')[1]
+					})
+				})
+				this.floorList = res.message
+			},
+
+
+
+
+		},
+
+
+
+
 	}
 </script>
 
 <style lang="scss">
+	swiper {
+		height: 330rpx;
 
+		.swiper-item,
+		image {
+			width: 100%;
+			height: 100%;
+		}
+	}
+
+	.nav-list {
+		display: flex;
+		justify-content: space-around;
+		margin: 15px 0;
+
+		.nav-img {
+			width: 128rpx;
+			height: 140rpx;
+		}
+	}
+
+	.floor-title {
+		height: 60rpx;
+		width: 100%;
+		display: flex;
+	}
+
+	.right-img-box {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-around;
+	}
+
+	.floor-img-box {
+		display: flex;
+		padding-left: 10rpx;
+	}
 </style>
